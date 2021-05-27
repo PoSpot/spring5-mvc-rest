99d6a068138a25c51dbe3a1aff3748bdc10f936e
Po <pmarinova@gmail.com>
2021-05-27 08:17:58 +0300

    request mapping fix

--- src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/customers/")
+@RequestMapping({"/api/v1/customers/", "/api/v1/customers"})
 public class CustomerController {
 
     private final CustomerService customerService;
